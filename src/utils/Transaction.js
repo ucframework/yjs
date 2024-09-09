63da384e56b7bd78ab4d0416219df899fdc6fde7
Bruno Silva <bm222gf@student.lnu.se>
2024-09-09 16:36:50 +0100

    Fix cleanupTransactions infinite loop

--- src/utils/Transaction.js
+++ src/utils/Transaction.js
@@ -258,7 +258,7 @@ export const tryGc = (ds, store, gcFilter) => {
  * @param {number} i
  */
 const cleanupTransactions = (transactionCleanups, i) => {
-  if (i < transactionCleanups.length) {
+  while (i < transactionCleanups.length) {
     const transaction = transactionCleanups[i]
     const doc = transaction.doc
     const store = doc.store
@@ -384,12 +384,12 @@ const cleanupTransactions = (transactionCleanups, i) => {
         subdocsRemoved.forEach(subdoc => subdoc.destroy())
       }
 
-      if (transactionCleanups.length <= i + 1) {
+      if (transactionCleanups.length <= ++i) {
         doc._transactionCleanups = []
         doc.emit('afterAllTransactions', [doc, transactionCleanups])
-      } else {
-        cleanupTransactions(transactionCleanups, i + 1)
-      }
+      }// else {
+      //  cleanupTransactions(transactionCleanups, i + 1)
+      //}
     }
   }
 }
